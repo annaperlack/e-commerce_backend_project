53488655146f0df9a25b402bbdec5cd02bfb9ca3
Anna Perlack <annaperlack@gmail.com>
2023-01-31 19:29:21 -0500

    updated models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+
+      }
+    },
+    category_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
